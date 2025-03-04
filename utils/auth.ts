852d1b0240a1cf9bb512355af34dde7b72513d2a
juzcallmekaushik <76829835+juzcallmekaushik@users.noreply.github.com>
2025-03-04 08:44:13 +0800

    Pool -> Client

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,16 +1,16 @@
 import NextAuth from "next-auth";
 import PostgresAdapter from "@auth/pg-adapter";
 import Discord from "next-auth/providers/discord";
-import { Pool } from "@neondatabase/serverless";
+import { Client } from "@neondatabase/serverless";
 
 export const runtime = "nodejs";
 
-const pool = new Pool({
-connectionString: process.env.DATABASE_URL, // Use single connection string for Neon
-ssl: true,
+const client = new Client({
+    connectionString: process.env.DATABASE_URL, // Use single connection string for Neon
+    ssl: true,
 });
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-adapter: PostgresAdapter(pool),
-providers: [Discord],
+    adapter: PostgresAdapter(Client),
+    providers: [Discord],
 });
