4bb59f2482df668c5ceca306be077f5da8426688
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-06-21 19:55:44 +0400

    add bg colour to header

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./navbar";
 
 export default function Header() {
   return (
-    <div className="sticky top-0 flex flex-row items-center justify-between px-6 py-4 min-w-full">
+    <div className="sticky top-0 flex flex-row items-center justify-between px-6 py-4 min-w-full bg-[#0a0a0a]">
       <div className="flex flex-row items-center gap-4 font-bold">
         <Image src="/logo.png" alt="Logo" width={32} height={32} />
         r/IGCSE
