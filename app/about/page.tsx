61b79b0db428bfd7442328521e743acdc2ad882a
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-06-21 19:48:31 +0400

    new event in timeline

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,7 +11,6 @@ export default function About() {
                             <img src="logo.png" className="w-16 h-16" />
                             <h1 className="text-2xl font-bold underline decoration-red-500 decoration-2 underline-offset-8">Our Mission</h1>
                         </div>
-                        <h2 className="text-sm italic text-gray-400 mt-1">Since 2019.</h2>
                         <p className="mt-2 text-base text-white-500 text-left">
                             r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
                             Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
@@ -26,7 +25,6 @@ export default function About() {
                             <img src="logo.png" className="w-16 h-16" />
                             <h1 className="text-2xl font-bold underline decoration-amber-500 decoration-2 underline-offset-8">Resource Repository</h1>
                         </div>
-                        <h2 className="text-sm italic text-gray-400 mt-1">Since 2019.</h2>
                         <p className="mt-2 text-base text-white-500 text-left">
                             r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
                             Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
@@ -41,7 +39,6 @@ export default function About() {
                             <img src="logo.png" className="w-16 h-16" />
                             <h1 className="text-2xl font-bold underline decoration-red-500 decoration-2 underline-offset-8">Our Mission</h1>
                         </div>
-                        <h2 className="text-sm italic text-gray-400 mt-1">Since 2019.</h2>
                         <p className="mt-2 text-base text-white-500 text-left">
                             r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
                             Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
@@ -72,6 +69,12 @@ export default function About() {
                                 <p className="bg-neutral-secondary-medium text-heading text-base font-medium px-1.5 py-0.5 rounded">June, 2021</p>
                                 <h3 className="flex items-center mb-1 text-lg font-semibold text-heading my-2">Discord Partner Status</h3>
                                 <p className="mb-4 text-body">Discord officially recognises our community and r/IGCSE becomes a Discord Partner.</p>
+                            </li>
+                            <li className="mb-10 ms-6">
+                                <span className="bg-red-500 absolute flex items-center justify-center w-6 h-6 bg-brand-softer rounded-full -inset-s-3 ring-8 ring-buffer ring-red-500"></span>
+                                <p className="bg-neutral-secondary-medium text-heading text-base font-medium px-1.5 py-0.5 rounded">September 14, 2025</p>
+                                <h3 className="flex items-center mb-1 text-lg font-semibold text-heading my-2">50,000 Server Members!</h3>
+                                <p className="mb-4 text-body">The r/IGCSE Discord Server hits 50,000 server members!.</p>
                             </li>
                         </ol>
                     </div>
