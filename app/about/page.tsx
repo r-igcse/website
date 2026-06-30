718dd27d0aa31f60fe5799c2f0f99e3b0dbf5ed1
NotCosmo <cosmin.dulgheru.alex@gmail.com>
2026-06-30 16:37:57 +0400

    improve code & replace 'contact the team' with opportunities

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,49 +7,48 @@ export default function About() {
             <div className="flex w-full gap-16">
                 <div className="flex flex-col gap-6 w-2/3">
                     <div className="bg-[#141417] border-t-4 border-red-500 p-4 rounded-lg">
-                        <div className="flex items-center gap-4">
-                            <img src="logo.png" className="w-16 h-16" />
-                            <h1 className="text-2xl font-bold underline decoration-red-500 decoration-2 underline-offset-8">Our Mission</h1>
+                        <div className="flex items-center gap-1">
+                            <img src="flag.png" className="w-16 h-16" />
+                            <h1 className="text-2xl font-bold">Our Mission</h1>
                         </div>
-                        <p className="mt-2 text-base text-white-500 text-left">
+                        <p className="mt-2 text-base text-white-500 text-left mr-6">
                             r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
-                            Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
+                            Our <a href="https://discord.gg/igcse" className="text-blue-300 underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
                             <br />
                             <br />
                             We are completely student-run, and all resources displayed have been created by other IGCSE & AS/A Level students. r/IGCSE is free and will always remain free.
                         </p>
                     </div>
 
-                    <div className="bg-[#141417] border-t-4 border-amber-500 p-4 rounded-lg">
-                        <div className="flex items-center gap-4">
-                            <img src="logo.png" className="w-16 h-16" />
-                            <h1 className="text-2xl font-bold underline decoration-amber-500 decoration-2 underline-offset-8">Resource Repository</h1>
+                    <div className="bg-[#141417] border-t-4 border-[#f7d2a5] p-4 rounded-lg">
+                        <div className="flex items-center gap-1">
+                            <img src="book1.png" className="w-16 h-16" />
+                            <h1 className="text-2xl font-bold">Resource Repository</h1>
                         </div>
-                        <p className="mt-2 text-base text-white-500 text-left">
-                            r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
-                            Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
+                        <p className="mt-2 text-base text-white-500 text-left mr-6">
+                            r/IGCSE has over 40+ resources published by our wonderful server members. These resources are free to view on our <a href="/resources" className="text-blue-300 underline decoration-white-300 underline-offset-4">resource repository</a>.
                             <br />
                             <br />
-                            We are completely student-run, and all resources displayed have been created by other IGCSE & AS/A Level students. r/IGCSE is free and will always remain free.
+                            If`` you find any mistakes within official r/IGCSE published resources, please do not hesitate to contact us through igcseresources.1@gmail.com.
                         </p>
                     </div>
 
-                    <div className="bg-[#141417] border-t-4 border-red-500 p-4 rounded-lg">
-                        <div className="flex items-center gap-4">
-                            <img src="logo.png" className="w-16 h-16" />
-                            <h1 className="text-2xl font-bold underline decoration-red-500 decoration-2 underline-offset-8">Our Mission</h1>
+                    <div className="bg-[#141417] border-t-4 border-blue-300 p-4 rounded-lg">
+                        <div className="flex items-center gap-1">
+                            <img src="team1.png" className="w-16 h-16" />
+                            <h1 className="text-2xl font-bold decoration-blue-300 decoration-2 underline-offset-8">Opportunities</h1>
                         </div>
                         <p className="mt-2 text-base text-white-500 text-left">
-                            r/IGCSE caters to students around the globe, providing both IGCSE & AS/A Level notes, subject guides, study sessions, and more.
-                            Our <a href="https://discord.gg/igcse" className="underline decoration-white-300 underline-offset-4">Discord Server</a> is focused on providing subject-specific channels in which you or other students may ask our verified Subject Helpers for assistance.
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus neque corrupti ratione rem eius ipsam sapiente cupiditate vel facere magnam dolorem, id molestiae velit aperiam voluptas pariatur omnis illo harum.
                             <br />
                             <br />
-                            We are completely student-run, and all resources displayed have been created by other IGCSE & AS/A Level students. r/IGCSE is free and will always remain free.
+                            <br />
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, porro? Minus vero magnam vitae laudantium facere, officia voluptatem enim, nesciunt voluptatum repudiandae ipsa velit animi corporis ad adipisci expedita iste.
                         </p>
                     </div>
                 </div>
 
-                <div className="w-1/3 self-start mt-2">
+                <div className="w-1/3 mt-2">
                     <div>
                         <ol className="relative border-s-2 border-red-500">
                             <li className="mb-10 ms-6">
